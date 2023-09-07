d30f3a433d366f87c1ef456301887e8614878671
Bill Crossley <efpophis@efpophis.net>
2023-09-07 12:17:27 -0400

    updated scripts to reflect deployment

--- socat_amp.sh
+++ socat_amp.sh
@@ -1,6 +1,6 @@
-# amp relay
-socat tcp-listen:51000,reuseaddr,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
+#!/bin/bash
 
-# tuner
-#socat tcp-listen:50000,reuseaddr,fork file:/dev/ttyUSB1,nonblock,waitlock=/var/run/tty1.lock,b38400,raw,echo=0 &
+# amp relay
+/usr/bin/socat tcp-listen:51000,reuseaddr,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
+
 
--- socat_tuner.sh
+++ socat_tuner.sh
@@ -1,6 +1,5 @@
-# amp relay
-#socat tcp-listen:51000,reuseaddr,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
+#!/bin/bash
 
 # tuner
-socat tcp-listen:50000,reuseaddr,fork file:/dev/ttyUSB1,nonblock,waitlock=/var/run/tty1.lock,b38400,raw,echo=0
+/usr/bin/socat tcp-listen:50000,reuseaddr,fork file:/dev/ttyUSB1,nonblock,waitlock=/var/run/tty1.lock,b38400,raw,echo=0
 
