bfd78a767af28c27e2e154029ab117591964e37a
Bill Crossley <efpophis@efpophis.net>
2023-10-06 15:42:26 -0400

    added ignoreeof option

--- socat_amp.sh
+++ socat_amp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # amp relay
-/usr/bin/socat tcp-listen:51000,reuseaddr,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
+/usr/bin/socat tcp-listen:51000,reuseaddr,ignoreeof,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
 
 
--- socat_tuner.sh
+++ socat_tuner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # tuner
-/usr/bin/socat tcp-listen:50000,reuseaddr,fork file:/dev/ttyUSB1,nonblock,waitlock=/var/run/tty1.lock,b38400,raw,echo=0
+/usr/bin/socat tcp-listen:50000,reuseaddr,ignoreeof,fork file:/dev/ttyUSB1,nonblock,waitlock=/var/run/tty1.lock,b38400,raw,echo=0
 
