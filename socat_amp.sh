6abc9e55c8af65a2ac5c9b7a813a45b728921eea
Bill Crossley <efpophis@efpophis.net>
2023-11-03 14:04:16 -0400

    Multiple Changes:   1. service now runs as nobody:dialout instead of root   2. service now specifies lock file location the right way   3. added iexten=0 to serial port options to keep it from      eating 0xF and 0x16 bytes.

--- socat_amp.sh
+++ socat_amp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # amp relay
-/usr/bin/socat tcp-listen:51000,reuseaddr,ignoreeof,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/tty0.lock,b9600,raw,echo=0
+/usr/bin/socat tcp-listen:51000,reuseaddr,ignoreeof,fork file:/dev/ttyUSB0,nonblock,waitlock=/var/run/socat_amp/tty0.lock,b9600,raw,echo=0,iexten=0
 
 
